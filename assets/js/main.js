2149cac0e9eb5df8b9863c7caa10e70b8242d923
vitorvbtf <vitorvbtf@gmail.com>
2023-07-31 15:06:12 -0300

    Alterações no main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
-const form = document.querySelector('form')
+function escopo(){
 
+let form = document.querySelector('form')
+
+function recebendoEvento(e) {
+    e.preventDefault();
+    const email = document.querySelector('#email')
+    const senha = document.querySelector('#senha')
+    
+    console.log(`email: ${email.value}, senha: ${senha.value}`);
+}
+
+
+
+form.addEventListener('submit', recebendoEvento)
+}
+escopo()
